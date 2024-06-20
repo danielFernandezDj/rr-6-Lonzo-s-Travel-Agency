277821ce0721c27673159542589162cdce033fe0
Daniel Fernandez <mdanif9728@gmail.com>
2024-06-19 19:40:06 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Lonzo's Travel Agency</h1>
+
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
           <AboutUs />
