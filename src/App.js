5ef82937a7160da6dcec26897878a44e4bda2c18
Daniel Fernandez <mdanif9728@gmail.com>
2024-06-19 21:10:18 -0700

    well inplement the Spiner and Lazy functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
-import AboutUs from './components/AboutUs'
-import Packages from './components/Packages'
+// import Loader from 'react-loader-spinner'
+import { Discuss } from 'react-loader-spinner'
+
+// Lazy:
+const AboutUs = React.lazy(() => import('./components/AboutUs'))
+const Packages = React.lazy(() => import('./components/Packages'))
 
 function App() {
   return (
     <div className="App">
       <h1>Lonzo's Travel Agency</h1>
-
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
+        {/* <Suspense fallback={<h1>Loading...</h1>}> */}
+        <Suspense fallback={(<Discuss
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="discuss-loading"
+          wrapperStyle={{}}
+          wrapperClass="discuss-wrapper"
+          color="#fff"
+          backgroundColor="#F4442E"
+        />)}>
           <AboutUs />
           <Packages />
+        </Suspense>
       </div>
     </div>
   );
